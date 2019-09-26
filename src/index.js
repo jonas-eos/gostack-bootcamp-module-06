009ff19a1ef3c8158dde696f446cc092e8bfac37
Jonas Evangelista <jonas.eos@outlook.com>
2019-09-25 23:50:48 -0300

    chore: status bar

--- src/index.js
+++ src/index.js
@@ -2,12 +2,17 @@
  * Main app page
  */
 import React from 'react';
-import { View } from 'react-native';
+import { StatusBar } from 'react-native';
 
-import Reactotron from 'reactotron-react-native';
+// import Reactotron from 'reactotron-react-native';
 import Routes from './routes';
 
 // Send a reactotron log
-const App = () => <Routes />;
+const App = () => (
+  <>
+    <StatusBar barStyle="light-content" backgroundColor="#E8A87C" />
+    <Routes />
+  </>
+);
 
 export default App;
