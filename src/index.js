9cc92a197307854892e29b84d58650507205b2df
Jonas Evangelista <jonas.eos@outlook.com>
2019-09-25 22:54:21 -0300

    chore: basic install of navigator

--- src/index.js
+++ src/index.js
@@ -6,27 +6,7 @@ import { StyleSheet, View, Text } from 'react-native';
 
 import Reactotron from 'reactotron-react-native';
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-});
-
 // Send a reactotron log
-Reactotron.log('Reactotron ok');
-const App = () => (
-  <View style={styles.container}>
-    <Text style={styles.welcome}>Welcome to React Native!</Text>
-    <Text style={styles.welcome}>Jonas!</Text>
-  </View>
-);
+const App = () => <View />;
 
 export default App;
