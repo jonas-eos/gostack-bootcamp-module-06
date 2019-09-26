1e2205f03436723a37e8f2de42d05430c23ea0ad
Jonas Evangelista <jonas.eos@outlook.com>
2019-09-26 00:32:52 -0300

    chore: styled-components

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { Container } from './styles';
 
 /**
  * @const Main
  * @type {component}
  * @default
  */
-const Main = () => <View />;
+const Main = () => <Container />;
 
 Main.navigationOptions = {
   title: 'Users',
---
+++ src/pages/Main/styles.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components/native';
+
+/**
+ * @const Container
+ * @type {StyledComponents}
+ */
+export const Container = styled.View`
+  flex: 1;
+  padding: 30px;
+`;
