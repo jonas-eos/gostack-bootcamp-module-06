4882d3153c9c4596c1fedfd42b45102e98ef1bb1
Jonas Evangelista <jonas.eos@outlook.com>
2019-09-26 21:56:49 -0300

    chore: Form style

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,12 +1,23 @@
 import React from 'react';
-import { Container } from './styles';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
+import { Container, Form, Input, SubmitButton } from './styles';
 
 /**
  * @const Main
  * @type {component}
  * @default
  */
-const Main = () => <Container />;
+const Main = () => (
+  <Container>
+    <Form>
+      <Input autoCorrect={false} autoCapitalize="none" placeholder="Add user" />
+      <SubmitButton>
+        <Icon name="add" size={20} color="#FFF" />
+      </SubmitButton>
+    </Form>
+  </Container>
+);
 
 Main.navigationOptions = {
   title: 'Users',
--- src/pages/Main/styles.js
+++ src/pages/Main/styles.js
@@ -1,10 +1,50 @@
 import styled from 'styled-components/native';
+import { RectButton } from 'react-native-gesture-handler';
 
 /**
  * @const Container
- * @type {StyledComponents}
+ * @type {View}
  */
 export const Container = styled.View`
   flex: 1;
   padding: 30px;
+`;
+
+/**
+ * @const Form
+ * @type {View}
+ */
+export const Form = styled.View`
+  flex-direction: row;
+  padding-bottom: 20px;
+  border-bottom-width: 1px;
+  border-color: #eee;
+`;
+
+/**
+ * @const Input
+ * @type {TextInput}
+ */
+export const Input = styled.TextInput.attrs({
+  placeholderTextColor: '#999',
+})`
+  flex: 1;
+  height: 40px;
+  background: #eee;
+  border-radius: 4px;
+  padding: 0 15px;
+  border: 1px solid #eee;
+`;
+
+/**
+ * @const SubmitButton
+ * @type {RectButton}
+ */
+export const SubmitButton = styled(RectButton)`
+  justify-content: center;
+  align-items: center;
+  background: #e8a87c;
+  border-radius: 4px;
+  margin-left: 10px;
+  padding: 0 12px;
 `;
