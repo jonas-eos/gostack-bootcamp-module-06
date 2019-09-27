3f558e4530851ae15620c5223c401b162d2916f3
Jonas Evangelista <jonas.eos@outlook.com>
2019-09-26 22:24:28 -0300

    chore: pre api-loader

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
+import Reactotron from 'reactotron-react-native';
 import { Container, Form, Input, SubmitButton } from './styles';
 
 /**
@@ -8,19 +9,43 @@ import { Container, Form, Input, SubmitButton } from './styles';
  * @type {component}
  * @default
  */
-const Main = () => (
-  <Container>
-    <Form>
-      <Input autoCorrect={false} autoCapitalize="none" placeholder="Add user" />
-      <SubmitButton>
-        <Icon name="add" size={20} color="#FFF" />
-      </SubmitButton>
-    </Form>
-  </Container>
-);
+export default class Main extends Component {
+  constructor() {
+    super();
+    this.state = {
+      newUser: '',
+      users: [],
+    };
+  }
+
+  handleAddUser = () => {
+    const { newUser } = this.state;
+    Reactotron.log(`${newUser}`);
+  };
+
+  render() {
+    const { users, newUser } = this.state;
+    return (
+      <Container>
+        <Form>
+          <Input
+            autoCorrect={false}
+            autoCapitalize="none"
+            placeholder="Add user"
+            value={newUser}
+            onChangeText={text => this.setState({ newUser: text })}
+            returnKeyType="send"
+            onSubmitEditing={this.handleAddUser}
+          />
+          <SubmitButton onPress={this.handleAddUser}>
+            <Icon name="add" size={20} color="#FFF" />
+          </SubmitButton>
+        </Form>
+      </Container>
+    );
+  }
+}
 
 Main.navigationOptions = {
   title: 'Users',
 };
-
-export default Main;
